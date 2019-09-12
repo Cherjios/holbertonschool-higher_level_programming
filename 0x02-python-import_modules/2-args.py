a867b462b62a228a4f5bb207875a3b6562aeff7e
Sergio Lopez <slopez0286@gmail.com>
2019-09-12 22:18:17 +0000

    Arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)-1
+    print('{} argument{}{}'
+          .format(n, "" if n is 1 else "s", "." if n is 0 else":"))
+    for i in range(len(sys.argv) - 1):
+        print('{}:{}'.format(i + 1, sys.argv[i + 1]))
