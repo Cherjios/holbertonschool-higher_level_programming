526eb26110ea66aefd191991cf44d90bc40b5a47
Sergio Lopez <slopez0286@gmail.com>
2019-09-17 18:31:25 +0000

    replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+def replace_in_list(my_list, idx, element):
+    num = len(my_list)
+    if idx < 0:
+        return None
+    if idx > num:
+        return None
+    my_list[idx] = element
+    return my_list
