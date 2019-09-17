fab168e85696a148460c90e0f69ead44bd1ef6ca
Sergio Lopez <slopez0286@gmail.com>
2019-09-17 20:18:22 +0000

    Repalce

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     num = len(my_list)
-    if idx < 0:
-        return None
-    if idx > num:
+    if idx < 0 or idx > num:
         return None
     my_list[idx] = element
     return my_list
