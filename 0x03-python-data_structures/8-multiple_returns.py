11e1a698d541bd5ee1bcb46222b1fbf7cd39d5fa
Sergio Lopez <slopez0286@gmail.com>
2019-09-17 23:49:54 +0000

    mul

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        a1 = None
+        return None
     a1 = len(sentence)
-    b1 = sentence[0:1]
+    b1 = sentence[0]
     return(a1, b1)
