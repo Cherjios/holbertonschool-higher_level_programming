aedd8e3b4bae001e27aedc905a33f3e1684d841e
Sergio Lopez <slopez0286@gmail.com>
2019-09-18 03:02:54 +0000

    maxi3

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,9 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max = 0
-    for i in my_list:
-        if i > max:
-            max = i
-    return (max)
+    else:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return (max)
