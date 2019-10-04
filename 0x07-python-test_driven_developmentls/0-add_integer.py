48c398c492b54d5f1340a86c21f4f89d193b2f6e
Sergio Lopez <slopez0286@gmail.com>
2019-10-04 22:11:06 +0000

    Add all the files

---
+++ 0x07-python-test_driven_developmentls/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for add_integer method."""
+
+
+def add_integer(a, b=98):
+    """ Add integer returns the addition of a and b
+
+    Args:
+    	a: The first integer.
+	b: The second integer.
+
+    Returns: The sume of  int a and int b """
+
+    if not isinstance (a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return (a + b )
+
+if __name__=="__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
+
---
+++ 0x07-python-test_driven_developmentls/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module Prints a square with the character # """
+
+def print_square(size):
+    """Method -  printing a square with the character #"""
+
+    """Arg:
+        size: takes an integer as a value, otherwise raise a TypeError
+        with the message size must be an integer
+       
+        size les than 0, ValueError with message size must be >= 0
+        size is a float and less than 0, raise a TypeError with message
+        size must be an integer """
+
+    if not isinstance(size, int) or isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        return
+    else:
+        print('\n'.join(['#' * size for j in range(size)]))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
+    
