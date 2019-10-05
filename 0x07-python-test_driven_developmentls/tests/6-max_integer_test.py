36c7c4998d04e4fe1c8cd13c37f99b373b708dcf
Sergio Lopez <slopez0286@gmail.com>
2019-10-05 00:40:32 +0000

    Add unittest

---
+++ 0x07-python-test_driven_developmentls/tests/6-max_integer_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """unittest for max_integer([...])"""
+
+    def test_no_arg(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer(), None)
+
+    def test_empty_list(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_one(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([98]), 98)
+
+    def test_identical(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+
+    def test_list_with_only_ints(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+   
+    def test_list_with_not_an_int(self):
+        with self.assertRaises(TypeError):
+            max_integer(['a', 2, 3, 4])
+
+
+if __name__ == '__main__':
+    unittest.main()
