12d3fc21b1b7dc080e431ee08b36690b228066aa
Sergio Lopez <slopez0286@gmail.com>
2019-10-06 21:11:56 +0000

    Fix print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """ This module Prints a square with the character # """
 
+
 def print_square(size):
     """Method -  printing a square with the character #"""
 
     """Arg:
         size: takes an integer as a value, otherwise raise a TypeError
         with the message size must be an integer
-       
         size les than 0, ValueError with message size must be >= 0
         size is a float and less than 0, raise a TypeError with message
         size must be an integer """
 
-    if not isinstance(size, int) or isinstance(size, float):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
@@ -24,4 +24,3 @@ def print_square(size):
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/4-print_square.txt")
-    
