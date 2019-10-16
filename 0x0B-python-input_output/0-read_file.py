fc33366f79fbc21e55b4f60a271cad1f9be9c169
Sergio Lopez <slopez0286@gmail.com>
2019-10-16 20:25:02 +0000

    Read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to read a file"""
+
+
+def read_file(filename=""):
+    """Read a text file UTF8 and print it to stdout"""
+    with open(filename, 'r') as f:
+        file_read = f.read()
+    print(file_read, end="")
