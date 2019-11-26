020c3ee62932c3cd980626d90db4dfb865831bea
cherjiosclear <slopez0286@gmail.com>
2019-11-26 04:24:48 +0000

    Adding 14 4

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,4 @@
- -- Script that computes the score average of all records in the table
- -- The result name should be average
-SELECT AVG(score) average FROM second_table;
+-- Removes all records with a score <= 5 in the table second_table
+DELETE FROM second_table
+WHERE 
+    score <= 5;
