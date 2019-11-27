bdbb54ee7f82c1c944c1c2fab5840d6797c6554c
cherjiosclear <slopez0286@gmail.com>
2019-11-27 04:58:45 +0000

    Adding 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all the shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
