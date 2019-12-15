506fe7939fe46c8980f55c9ef8b489727f8b692e
cherjiosclear <slopez0286@gmail.com>
2019-12-15 22:01:54 +0000

    adding 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-# Lsiting all states from data base hbtn_0e_0_usa
+""" Lsiting all states from data base hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
     username = sys.argv[1]
-    pssw = sys.argv[2]
-    datab_name = sys.argv[3]
+    psw = sys.argv[2]
+    db_name = sys.argv[3]
 
-    datab = MySQLdb.connect(host="localhost", port=3306, user=username,
-                            passwd=pssw, db=datab_name, charset="utf8")
-    cur = datab.cursor()
-    cur.execute("SELECT * FORM state ORDER BY id ASC")
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=psw,
+                         db=db_name,
+                         charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
-    datab.close()
+    db.close()
