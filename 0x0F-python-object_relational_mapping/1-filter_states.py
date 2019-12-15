4641e1a1267ccf3837b2ef829263ab61f15c9157
cherjiosclear <slopez0286@gmail.com>
2019-12-15 23:04:36 +0000

    Adding number 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Lsiting all states starting with N"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    pswd = sys.argv[2]
+    dname = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=pswd, db=dname, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE " +
+                "name LIKE BINARY 'N%' ORDER BY 'states.id' ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
