3b2b33d4067252f0ef84e534125087ac029f54eb
cherjiosclear <slopez0286@gmail.com>
2020-01-07 02:52:48 +0000

    Adding 6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,11 +4,11 @@
 
 def find_peak(list_of_integers):
     for i in range(len(list_of_integers)-1):
-        if list_of_integers[i+1] > list_of_integers[i] and
-        list_of_integers[i + 1] > list_of_integers[i + 2]:
+        if list_of_integers[i+1] > list_of_integers[i] and \
+                list_of_integers[i + 1] > list_of_integers[i + 2]:
             return list_of_integers[i + 1]
-        elif list_of_integers[i+1] == list_of_integers[i] or
-        list_of_integers[i + 1] == list_of_integers[i + 2]:
+        elif list_of_integers[i+1] == list_of_integers[i] or \
+                list_of_integers[i + 1] == list_of_integers[i + 2]:
             return list_of_integers[i + 1]
         else:
             continue
