2ed386a9222144987d8ed4189f324e561f8ef889
cherjiosclear <slopez0286@gmail.com>
2020-01-07 02:40:14 +0000

    Adding peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" unction that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    for i in range(len(list_of_integers)-1):
+        if list_of_integers[i+1] > list_of_integers[i] and
+        list_of_integers[i + 1] > list_of_integers[i + 2]:
+            return list_of_integers[i + 1]
+        elif list_of_integers[i+1] == list_of_integers[i] or
+        list_of_integers[i + 1] == list_of_integers[i + 2]:
+            return list_of_integers[i + 1]
+        else:
+            continue
