fed8f71d6eea82f10f8209c8513141d65066849e
cherjiosclear <slopez0286@gmail.com>
2020-01-13 22:07:30 +0000

    Adding 2-post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,5 +9,5 @@ from sys import argv
 
 if __name__ == "__main__":
     post_data = urllib.parse.urlencode({'email': argv[2]}).encode('ascii')
-    with urllib.request.urlopen(url = argv[1], data=post_data) as response:
+    with urllib.request.urlopen(url=argv[1], data=post_data) as response:
         print(response.read().decode('UTF-8'))
