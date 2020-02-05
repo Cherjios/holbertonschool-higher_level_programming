0847f141f66996c48afa410962c049734309d9d0
cherjiosclear <slopez0286@gmail.com>
2020-02-05 01:10:48 +0000

    Adding number7  2

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that prints x times "C is fun"
-const num = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 let i;
-if (isNaN(num)) {
+if (isNaN(x)) {
   console.log('Missing number of occurances');
 } else {
-  for (i = 0; i <= num; i++) {
+  for (i = 1; i <= x; i++) {
     console.log('C is fun');
   }
 }
