3625f1aa1882d8268793b40c3c28c986dd48e1fe
cherjiosclear <slopez0286@gmail.com>
2020-02-05 00:54:31 +0000

    Adding 6 2

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints “Javascript is amazing”:
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-var i;
+let i;
 for (i = 0; i < myVar.length; i++) {
   console.log(myVar[i]);
 }
