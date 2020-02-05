ac838ef3c7185b5228ca8925d62b54a4f0548cc1
cherjiosclear <slopez0286@gmail.com>
2020-02-05 03:38:39 +0000

    Adding squre 2

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const x = parseInt(process.argv[2]);
 let i, j, res;
 res = '';
 if (isNaN(x)) {
-  console.log('Missing number of occurances');
+  console.log('Missing size');
 } else {
   for (i = 1; i <= x; i++) {
     for (j = 1; j <= x; j++) {
