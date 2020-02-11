261b998d72e8ac89293152bbc401917dbb536d90
cherjiosclear <slopez0286@gmail.com>
2020-02-11 05:58:05 +0000

    Adding 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      return;
-    }
-    this.width = w;
-    this.height = h;
-  }
+const s = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends s {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
