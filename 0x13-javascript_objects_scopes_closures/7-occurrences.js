1cb52faddaea26a191dec83ab1e40987ba50b5d3
cherjiosclear <slopez0286@gmail.com>
2020-02-11 05:59:49 +0000

    Adding 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement){
-  return list.reduce((count, elem) => {
-    if (elem === searchElement){
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, el) => {
+    if (el === searchElement) {
       count++;
     }
     return count;
