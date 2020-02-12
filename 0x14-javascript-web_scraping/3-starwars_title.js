77657ee30051496bf5eb7f0118acdcfd9686054d
cherjiosclear <slopez0286@gmail.com>
2020-02-12 00:57:47 +0000

    Adding 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const myUrl = 'http://swapi.co/api/films/' + id;
+const request = require('request');
+request.get(myUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const movie = JSON.parse(body).movie;
+  console.log(movie);
+});
